b001e63014af3583654e68d4e053b9155530c90b
福島碧 <fukushimamid@gmail.com>
2021-06-19 16:29:26 +0900

    微調整

--- index0612.js
+++ index0612.js
@@ -2,13 +2,13 @@ for (let i = 2; i <= 100; i=i+2) {
     let body = ''
     
     if(i%15==0){
-        body=i+'回目。私はアホ犬なのでしょうかワン？';
+        body = i +'回目。私はアホ犬なのでしょうかワン？';
     }else if(i%3==0){
-        body=i+'回目。アホ';
+        body = i +'回目。アホ';
     }else if(i%5==0){
-        body=i+'回目。犬';
+        body = i +'回目。犬';
     }else{
-        body=i
+        body = i
     }
     console.log(body);
 }
